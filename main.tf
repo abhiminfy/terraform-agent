d846f4aff09ac0ca77b81dec12b0acebc7ae9a33
Abhinav Mishra <abhinav.mishra@minfytech.com>
2025-07-21 22:18:44 +0530

    Auto-push: Terraform code generated by AI

--- main.tf
+++ main.tf
@@ -1,93 +1,45 @@
-# Create an execution role for the Lambda function
-resource "aws_iam_role" "lambda_exec_role" {
-  name = "lambda_exec_role"
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "my-tf-test-bucket-website"
+  acl    = "private"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
+ website {
+    index_document = "index.html"
+    error_document = "error.html"
 
-# Attach the AWSLambdaBasicExecutionRole policy to the role
-resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-# Create a policy to allow logging to CloudWatch Logs
-resource "aws_iam_policy" "lambda_cloudwatch_policy" {
-  name = "lambda_cloudwatch_policy"
-
- policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "arn:aws:logs:*:*:*"
-    }
-  ]
-}
-EOF
-}
-
-
-# Attach the CloudWatch logging policy to the role
-resource "aws_iam_role_policy_attachment" "lambda_cloudwatch_attachment" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = aws_iam_policy.lambda_cloudwatch_policy.arn
-}
-
-
-
-# Create the Lambda function
-resource "aws_lambda_function" "example" {
-  filename      = null # No filename since we're using inline code
-  function_name = "my_lambda_function"
-  role          = aws_iam_role.lambda_exec_role.arn
-  handler       = "index.handler"
-  runtime = "nodejs18.x"
-
-  source_code_hash = base64sha256(data.archive_file.lambda_zip.output_base64sha256)
-  memory_size = 128
-  timeout = 30
-
-
-  data.archive_file.lambda_zip.output_path
-
-
-  data "archive_file" "lambda_zip" {
-    type        = "zip"
-    output_path = "${path.module}/lambda_function.zip"
-    source {
-      content  = <<EOF
-exports.handler = async (event) => {
-  console.log("Event:", JSON.stringify(event, null, 2));
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello from Lambda!" }),
-  };
-};
-EOF
-      filename = "index.js"
-    }
   }
 
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::my-tf-test-bucket-website/*"
+        }
+    ]
+}
+EOF
+}
 
+resource "aws_s3_bucket_object" "index" {
+  bucket = aws_s3_bucket.website_bucket.id
+  key    = "index.html"
+  source = "index.html" # Make sure this file exists in the same directory
+ content_type = "text/html"
+}
+
+
+resource "aws_s3_bucket_object" "error" {
+  bucket = aws_s3_bucket.website_bucket.id
+  key    = "error.html"
+  source = "error.html" # Make sure this file exists in the same directory
+  content_type = "text/html"
+}
+
+
+output "website_endpoint" {
+  value = aws_s3_bucket.website_bucket.website_endpoint
 }
